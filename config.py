c26c34428cc8d9ce78df7680f730d18d71de1aa7
Pontus Markström <pontus.markstrom@gmail.com>
2016-04-03 12:24:14 +0200

    Configuration for importing to include wanted files

--- config.py
+++ config.py
@@ -10,9 +10,12 @@ PASSWORD = 'admin'
 FILES_ROOT_DIRECTORY = 'x:/'
 #FILES_ROOT_DIRECTORY = 'files'
 
-# Black-listed file pattern in case sensitive.
+# Files are ignored when any of the blacklisted file patterns can be found in the file path.
+# Black-listed file patterns are case sensitive.
 BLACKLISTED_FILE_PATH_PATTERNS = ['Thumbs.db', 'nytt/', 'unsorted/', 'TN_', 'privat/']
 
+# If a file is not blacklisted a whitelisted test will be done.
+# A file will be added if its path ends with any of the specified patterns.
 # Specify an empty list to white-list all files (if not being black-listed before)
-# File path is made lower-case before scan (i.e. specify the file extension in lower-case).
-WHITELISTED_FILE_EXTENSIONS = ['.jpg', '.png', '.bmp', '.gif', '.avi', '.mpg', '.mp4']
+# Note: file path is made lower-case before scan (i.e. specify the file extension in lower-case).
+WHITELISTED_FILE_EXTENSIONS = ['.jpg', '.png', '.bmp', '.gif', '.avi', '.mpg', '.mp4', '.mkv']
