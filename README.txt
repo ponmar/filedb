53b57cd3b0bc72f1d9800d7dff154f773b53c856
Pontus Markstrom <pontus.markstrom@ericsson.com>
2016-04-04 08:07:28 +0200

    Documenation and configuration

--- README.txt
+++ README.txt
@@ -1,10 +1,10 @@
 Installation
 ============
-1. Install Python and pip
+1. Install Python and pip (installation instructions depends on your OS)
 2. Install Flask (web server) and Pillow (for reading date and time from JPEG images):
    >pip install flask
    >pip install pillow
-3. Edit configuration in flaskr.py
+3. Edit configuration in config.py
 
 
 Creating the database
@@ -14,7 +14,8 @@ python runserver.py --initdb
 
 Import files recursively (optional)
 ===================================
-python runserver.py --importdir <dir>
+* Start server
+* Access /import URL (until button added on web page)
 
 
 Starting the server
--- config.py
+++ config.py
@@ -12,10 +12,11 @@ FILES_ROOT_DIRECTORY = 'x:/'
 
 # Files are ignored when any of the blacklisted file patterns can be found in the file path.
 # Black-listed file patterns are case sensitive.
-BLACKLISTED_FILE_PATH_PATTERNS = ['Thumbs.db', 'nytt/', 'unsorted/', 'TN_', 'privat/']
+BLACKLISTED_FILE_PATH_PATTERNS = ['TN_', 'Thumbs.db', 'nytt/', 'unsorted/', 'privat/']
 
 # If a file is not blacklisted a whitelisted test will be done.
-# A file will be added if its path ends with any of the specified patterns.
-# Specify an empty list to white-list all files (if not being black-listed before)
+# A file will be added, during an import action, if its path ends with any of
+# the specified patterns. Specify an empty list to white-list all files (if
+# not being black-listed before)
 # Note: file path is made lower-case before scan (i.e. specify the file extension in lower-case).
-WHITELISTED_FILE_EXTENSIONS = ['.jpg', '.png', '.bmp', '.gif', '.avi', '.mpg', '.mp4', '.mkv', '.mov']
+WHITELISTED_FILE_EXTENSIONS = ['.jpg', '.png', '.bmp', '.gif', '.avi', '.mpg', '.mp4', '.mkv', '.mov', '.pdf']
