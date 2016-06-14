e106a02d5aa7e622d1d6e0bab2d6a8385a66ae17
Pontus Markstrom <pontus.markstrom@ericsson.com>
2016-06-14 12:26:45 +0200

    Updated Python code example for using the API

--- api/example.py
+++ api/example.py
@@ -1,6 +1,46 @@
+"""Python example code for fetching data from FileDB."""
 import requests
 
+BASE_URL = 'http://localhost:5000/'
+
+
+def get_json(url):
+    r = requests.get(url)
+    return r.json()
+
+
+def find_item(item_id, json_list):
+    for item in json_list:
+        if item['id'] == item_id:
+            return item
+    return None
+
+
 if __name__ == '__main__':
-    r = requests.get('http://localhost:5000/api/files')
-    files = r.json()
-    print(files)
+    files = get_json(BASE_URL + 'api/files')['files']
+    persons = get_json(BASE_URL + 'api/persons')['persons']
+    locations = get_json(BASE_URL + 'api/locations')['locations']
+    tags = get_json(BASE_URL + 'api/tags')['tags']
+
+    for file in files:
+        file_path = file['path']
+        file_description = file['description']
+        file_date = file['datetime']
+        file_person_names = []
+        file_location_names = []
+        file_tag_names = []
+
+        for person_id in file['persons']:
+            person = find_item(person_id, persons)
+            if person is not None:
+                file_person_names.append(person['name'])
+
+        for location_id in file['locations']:
+            location = find_item(location_id, locations)
+            if location is not None:
+                file_location_names.append(location['name'])
+
+        for tag_id in file['tags']:
+            tag = find_item(tag_id, tags)
+            if tag is not None:
+                file_tag_names.append(tag['name'])
