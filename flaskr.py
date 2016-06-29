a1e908d5c027988d4321ba093f945778bcb02d14
Pontus Markstrom <pontus.markstrom@ericsson.com>
2016-06-29 10:07:12 +0200

    Unicode fix for Python >= 3

--- flaskr.py
+++ flaskr.py
@@ -10,6 +10,16 @@ import jpegfile
 from config import *
 from werkzeug.routing import BaseConverter
 
+import sys
+if sys.version_info < (3,):
+    import codecs
+
+    def convert_to_unicode(x):
+        return codecs.unicode_escape_decode(x)[0]
+else:
+    def convert_to_unicode(x):
+        return x
+
 
 class IntListConverter(BaseConverter):
     regex = r'\d+(?:,\d+)*,?'
@@ -159,7 +169,7 @@ def api_import_files():
     num_imported_files = 0
     num_not_imported_files = 0
 
-    for root, directories, filenames in os.walk(unicode(FILES_ROOT_DIRECTORY)):
+    for root, directories, filenames in os.walk(convert_to_unicode(FILES_ROOT_DIRECTORY)):
         for filename in filenames:
             filename_with_path = os.path.join(root, filename)
             filename_with_path = update_path(filename_with_path)
