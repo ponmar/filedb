d91a9d4a6c550bf8077aa9b7755c3bac7d9fbec6
Pontus Markström <pontus.markstrom@gmail.com>
2018-05-29 21:08:45 +0200

    Add util for downloading files specified in an exported file list

---
+++ utils/file_downloader.py
@@ -0,0 +1,40 @@
+import requests
+import argparse
+
+TITLE = 'FileDB file downloader'
+FILENAME = '{index:0>3}_{file_id}.jpg'
+FILE_ID_SEPARATOR = ';'
+FILEDB_FILECONTENT_URL = '{}/api/filecontent/{}'
+
+
+def get_file(url):
+    r = requests.get(url)
+    if r.status_code != requests.codes.ok:
+        print('Server returned status code {}'.format(r.status_code))
+        return None
+    return r.content
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=TITLE)
+    parser.add_argument('server', help='Example: http://localhost')
+    parser.add_argument('files', help='Example: 20;3;2043')
+    args = parser.parse_args()
+
+    file_id_strs = args.files.split(FILE_ID_SEPARATOR)
+    file_ids = []
+    for file_id_str in file_id_strs:
+        if file_id_str:
+            file_ids.append(int(file_id_str))
+
+    filename_index = 1
+    for file_id in file_ids:
+        url = FILEDB_FILECONTENT_URL.format(args.server, file_id)
+        file_content = get_file(url)
+        if file_content is not None:
+            filename = FILENAME.format(index=filename_index, file_id=file_id)
+            with open(filename, 'wb') as f:
+                f.write(file_content)
+        else:
+            print('Could not download ' + url)
+        filename_index += 1
