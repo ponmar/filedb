41b6832b49ec315a0cc1c6cfdb9754dda7106f08
Pontus Markström <pontus.markstrom@gmail.com>
2019-11-17 13:07:18 +0100

    Util: minor FileDBCast refactoring

---
+++ utils/FileDbUtils/FileDbCast/StringContentToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace FileDbCast
+{
+    public class StringContentToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if (value != null && ((string)value).Length > 0)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
